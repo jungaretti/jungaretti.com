e64ba1cd982a218ddd507b43070fdb1c064362fb
JP Ungaretti <hello@jungaretti.com>
2021-06-28 17:51:51 -0500

    Add tailwindcss

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,9 @@
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addWatchTarget("src/css");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addWatchTarget("src/favicon.ico");
 
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addWatchTarget("src/assets");
-
-  eleventyConfig.addPassthroughCopy("src/favicon.ico");
-  eleventyConfig.addWatchTarget("src/favicon.ico");
 
   return {
     dir: {
