a9d4ebb9c77884ec2389afd9e0bf9095dec2b570
JP Ungaretti <hello@jungaretti.com>
2021-08-16 21:02:17 -0700

    Add and use Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
+const {fontFamily} = require('tailwindcss/defaultTheme');
+
 module.exports = {
   // mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', fontFamily.sans]
+      }
+    }
+  }
 };
