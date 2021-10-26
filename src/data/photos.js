3e2809b85774b7c257dd6657535083f2cbb889d6
JP Ungaretti <hello@jungaretti.com>
2021-10-25 20:19:42 -0700

    Grab photos from data file

--- src/data/photos.js
+++ src/data/photos.js
@@ -2,29 +2,33 @@ export const photos = [
   {
     image: {
       compressedUrl:
-        "https://badgerville.blob.core.windows.net/journal/cascadian-stars-web.jpg",
-      fullSizedUrl:
-        "https://badgerville.blob.core.windows.net/journal/cascadian-stars.jpg",
-    },
-    meta: {
-      title: "Riveder le Stelle",
-      description: null,
-      location: "North Cascades National Park",
-      date: new Date("2021-09-24"),
-    },
-  },
-  {
-    image: {
-      compressedUrl:
         "https://badgerville.blob.core.windows.net/journal/sunset-sailing-web.jpg",
       fullSizedUrl:
         "https://badgerville.blob.core.windows.net/journal/sunset-sailing.jpg",
+      alt: null,
     },
     meta: {
       title: "New Horizons",
       description: null,
       location: "Seattle, WA",
       date: new Date("2021-09-03"),
+      favorite: true,
+    },
+  },
+  {
+    image: {
+      compressedUrl:
+        "https://badgerville.blob.core.windows.net/journal/cascadian-stars-web.jpg",
+      fullSizedUrl:
+        "https://badgerville.blob.core.windows.net/journal/cascadian-stars.jpg",
+      alt: null,
+    },
+    meta: {
+      title: "Riveder le Stelle",
+      description: null,
+      location: "North Cascades National Park",
+      date: new Date("2021-09-24"),
+      favorite: true,
     },
   },
 ];
