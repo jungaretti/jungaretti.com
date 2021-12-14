470195fd5c6d419189d12fb58b5cf77697c5be8e
JP Ungaretti <hello@jungaretti.com>
2021-12-14 01:10:44 -0800

    Update photo references

--- src/data/photos.js
+++ src/data/photos.js
@@ -3,9 +3,8 @@ export const photos = [
     id: "new-horizons",
     src: {
       compressed:
-        "https://badgerville.blob.core.windows.net/journal/new-horizons-small.jpg",
-      original:
-        "https://badgerville.blob.core.windows.net/journal/new-horizons.jpg",
+        "https://badger.blob.core.windows.net/photos/new-horizons-small.jpg",
+      original: "https://badger.blob.core.windows.net/photos/new-horizons.jpg",
     },
     alt: "Boat sailing in front of sunset over the Olympic Mountains",
     meta: {
@@ -20,9 +19,9 @@ export const photos = [
     id: "riveder-le-stelle",
     src: {
       compressed:
-        "https://badgerville.blob.core.windows.net/journal/riveder-le-stelle-small.jpg",
+        "https://badger.blob.core.windows.net/photos/riveder-le-stelle-small.jpg",
       original:
-        "https://badgerville.blob.core.windows.net/journal/riveder-le-stelle.jpg",
+        "https://badger.blob.core.windows.net/photos/riveder-le-stelle.jpg",
     },
     alt: "Glimpse of the Milky Way hovering over the Cascades",
     meta: {
@@ -30,40 +29,6 @@ export const photos = [
       description: null,
       location: "North Cascades National Park",
       date: "September 2021",
-      favorite: true,
-    },
-  },
-  {
-    id: "4960-miles",
-    src: {
-      compressed:
-        "https://badgerville.blob.core.windows.net/journal/4960-miles-small.jpg",
-      original:
-        "https://badgerville.blob.core.windows.net/journal/4960-miles.jpg",
-    },
-    alt: "Friends standing underneat an arch at Bryce Canyon National Park",
-    meta: {
-      title: "4960 Miles",
-      description: null,
-      location: "Bryce Canyon National Park",
-      date: "January 2021",
-      favorite: true,
-    },
-  },
-  {
-    id: "sheepshead-and-stars",
-    src: {
-      compressed:
-        "https://badgerville.blob.core.windows.net/journal/sheepshead-and-stars-small.jpg",
-      original:
-        "https://badgerville.blob.core.windows.net/journal/sheepshead-and-stars.jpg",
-    },
-    alt: "Silhouette of friends standing atop a rocky hill",
-    meta: {
-      title: "Sheepshead and Stars",
-      description: null,
-      location: "Lake Powell",
-      date: "January 2021",
       favorite: true,
     },
   },
