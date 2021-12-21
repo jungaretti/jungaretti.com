353a79f5b042a67c0c941df1892b993f96380af1
JP Ungaretti <hello@jungaretti.com>
2021-12-21 16:53:03 -0600

    Add new photos

--- src/data/photos.js
+++ src/data/photos.js
@@ -3,7 +3,7 @@ export const photos = [
     id: "new-horizons",
     src: {
       compressed:
-        "https://badger.blob.core.windows.net/photos/new-horizons-small.jpg",
+        "https://badger.blob.core.windows.net/photos/new-horizons-preview.jpg",
       original: "https://badger.blob.core.windows.net/photos/new-horizons.jpg",
     },
     alt: "Boat sailing in front of sunset over the Olympic Mountains",
@@ -19,7 +19,7 @@ export const photos = [
     id: "riveder-le-stelle",
     src: {
       compressed:
-        "https://badger.blob.core.windows.net/photos/riveder-le-stelle-small.jpg",
+        "https://badger.blob.core.windows.net/photos/riveder-le-stelle-preview.jpg",
       original:
         "https://badger.blob.core.windows.net/photos/riveder-le-stelle.jpg",
     },
@@ -29,6 +29,38 @@ export const photos = [
       description: null,
       location: "North Cascades National Park",
       date: "September 2021",
+      favorite: true,
+    },
+  },
+  {
+    id: "george",
+    src: {
+      compressed:
+        "https://badger.blob.core.windows.net/photos/george-preview.jpg",
+      original: "https://badger.blob.core.windows.net/photos/george.jpg",
+    },
+    alt: "Orange and white cat",
+    meta: {
+      title: "George",
+      description: null,
+      location: "Under the Table",
+      date: "March 2015",
+      favorite: true,
+    },
+  },
+  {
+    id: "flames",
+    src: {
+      compressed:
+        "https://badger.blob.core.windows.net/photos/flames-preview.jpg",
+      original: "https://badger.blob.core.windows.net/photos/flames.jpg",
+    },
+    alt: "Buttery-smooth bonfire flames",
+    meta: {
+      title: "Flames",
+      description: null,
+      location: "Southern Michigan",
+      date: "September 2014",
       favorite: true,
     },
   },
