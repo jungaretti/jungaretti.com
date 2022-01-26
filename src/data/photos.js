6ffacf81a56c28870ab60c5ed159d00384e2448a
JP Ungaretti <hello@jungaretti.com>
2022-01-25 22:14:02 -0800

    Add new photo

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,15 +1,31 @@
 const blobBase = "https://badger.blob.core.windows.net/photos";
 
-const blobUri = (id) => `${blobBase}/${id}.jpg`;
-const blobPreviewUri = (id) => `${blobBase}/${id}-preview.jpg`;
+function buildPhoto({ id, alt, meta }) {
+  return {
+    id,
+    src: {
+      compressed: `${blobBase}/${id}.jpg`,
+      original: `${blobBase}/${id}-preview.jpg`,
+    },
+    alt,
+    meta,
+  };
+}
 
 export const photos = [
-  {
-    id: "new-horizons",
-    src: {
-      compressed: blobPreviewUri("new-horizons"),
-      original: blobUri("new-horizons"),
+  buildPhoto({
+    id: "raggi-del-pianeta",
+    alt: "Golden sunset over a snowy mountain",
+    meta: {
+      title: "Raggi del Pianeta",
+      description: null,
+      location: "North Cascades National Park",
+      date: "January 2021",
+      favorite: true,
     },
+  }),
+  buildPhoto({
+    id: "new-horizons",
     alt: "Boat sailing in front of sunset over the Olympic Mountains",
     meta: {
       title: "New Horizons",
@@ -18,13 +34,9 @@ export const photos = [
       date: "September 2021",
       favorite: true,
     },
-  },
-  {
+  }),
+  buildPhoto({
     id: "riveder-le-stelle",
-    src: {
-      compressed: blobPreviewUri("riveder-le-stelle"),
-      original: blobUri("riveder-le-stelle"),
-    },
     alt: "Glimpse of the Milky Way hovering over the Cascades",
     meta: {
       title: "Riveder le Stelle",
@@ -33,35 +45,16 @@ export const photos = [
       date: "September 2021",
       favorite: true,
     },
-  },
-  {
-    id: "george",
-    src: {
-      compressed: blobPreviewUri("george"),
-      original: blobUri("george"),
-    },
+  }),
+  buildPhoto({
+    id: "mio-gatto",
     alt: "Orange and white cat",
     meta: {
-      title: "George",
+      title: "Mio Gatto",
       description: null,
       location: "Under the Table",
       date: "March 2015",
       favorite: true,
     },
-  },
-  {
-    id: "flames",
-    src: {
-      compressed: blobPreviewUri("flames"),
-      original: blobUri("flames"),
-    },
-    alt: "Buttery-smooth bonfire flames",
-    meta: {
-      title: "Flames",
-      description: null,
-      location: "Southern Michigan",
-      date: "September 2014",
-      favorite: true,
-    },
-  },
+  }),
 ];
