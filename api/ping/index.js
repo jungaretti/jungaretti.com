bf3a6069c8d698b910b50ee539c7a6591fdd00eb
JP Ungaretti <hello@jungaretti.com>
2022-04-20 21:41:20 -0700

    Switch to js functions

--- api/ping/index.js
+++ api/ping/index.js
@@ -1,7 +1,6 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
@@ -11,7 +10,4 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
-
-};
-
-export default httpTrigger;
+}
