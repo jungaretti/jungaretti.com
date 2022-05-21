a5663fc2cbe39cc09e2975a71e84490cc4ba182e
JP Ungaretti <hello@jungaretti.com>
2022-05-20 22:11:06 -0700

    Update tons of packages

--- .devcontainer/scripts/postcreate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Install act from https://github.com/nektos/act
-curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sh
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
-export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    site: "https://www.jungaretti.com",
-    sitemap: true, // Generate sitemap (set to "false" to disable)
-  },
-  devOptions: {
-    // port: 3000,         // The port to run the dev server on.
-    tailwindConfig: "./tailwind.config.js",
-  },
-  renderers: ["@astrojs/renderer-react"],
-};
+import { defineConfig } from 'astro/config';
+
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind()]
+});
