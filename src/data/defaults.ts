16330b885175ee63f9ea46e882ab2de5999468f6
JP Ungaretti <hello@jungaretti.com>
2022-07-26 02:51:58 +0000

    Extract defaults

---
+++ src/data/defaults.ts
@@ -0,0 +1,6 @@
+export const defaults = {
+  title: "JP Ungaretti",
+  description:
+    "I'm a software engineer, photographer, and cheese curd connoisseur. Check out my recent photos or get in touch with me.",
+  permalink: "https://www.jungaretti.com/",
+};
