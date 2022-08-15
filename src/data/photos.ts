bbba78ac34bedddbdfe8dcf3aff5dff27dd68dc3
JP Ungaretti <hello@jungaretti.com>
2022-08-14 20:29:13 -0700

    Add new photos

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -15,13 +15,37 @@ export class Photo {
 
 export const photos: Photo[] = [
   {
+    id: "lone-moose",
+    alt: "Mountain peak in Grand Teton National Park",
+    src: {
+      compressed:
+        "https://badger.blob.core.windows.net/images/lone-moose-preview.jpg",
+      original: "https://badger.blob.core.windows.net/images/lone-moose.jpg",
+    },
+    title: "Lone Moose",
+    location: "Grand Teton National Park",
+    date: new Date("2022-07-03"),
+  },
+  {
+    id: "our-universe",
+    alt: "Pristine view of our Galactric Center over Yellowstone National Park",
+    src: {
+      compressed:
+        "https://badger.blob.core.windows.net/images/our-universe-preview.jpg",
+      original: "https://badger.blob.core.windows.net/images/our-universe.jpg",
+    },
+    title: "Our Universe",
+    location: "Yellowstone National Park",
+    date: new Date("2022-07-03"),
+  },
+  {
     id: "raggi-del-pianeta",
     alt: "Golden sunset over a snowy mountain",
     src: {
       compressed:
-        "https://badger.blob.core.windows.net/photos/raggi-del-pianeta-preview.jpg",
+        "https://badger.blob.core.windows.net/images/raggi-del-pianeta-preview.jpg",
       original:
-        "https://badger.blob.core.windows.net/photos/raggi-del-pianeta.jpg",
+        "https://badger.blob.core.windows.net/images/raggi-del-pianeta.jpg",
     },
     title: "Raggi del Pianeta",
     location: "North Cascades National Park",
@@ -32,8 +56,8 @@ export const photos: Photo[] = [
     alt: "Boat sailing in front of sunset over the Olympic Mountains",
     src: {
       compressed:
-        "https://badger.blob.core.windows.net/photos/new-horizons-preview.jpg",
-      original: "https://badger.blob.core.windows.net/photos/new-horizons.jpg",
+        "https://badger.blob.core.windows.net/images/new-horizons-preview.jpg",
+      original: "https://badger.blob.core.windows.net/images/new-horizons.jpg",
     },
     title: "New Horizons",
     location: "Golden Gardens Park",
@@ -44,9 +68,9 @@ export const photos: Photo[] = [
     alt: "Glimpse of the Milky Way hovering over the Cascades",
     src: {
       compressed:
-        "https://badger.blob.core.windows.net/photos/riveder-le-stelle-preview.jpg",
+        "https://badger.blob.core.windows.net/images/riveder-le-stelle-preview.jpg",
       original:
-        "https://badger.blob.core.windows.net/photos/riveder-le-stelle.jpg",
+        "https://badger.blob.core.windows.net/images/riveder-le-stelle.jpg",
     },
     title: "Riveder le Stelle",
     location: "North Cascades National Park",
@@ -57,8 +81,8 @@ export const photos: Photo[] = [
     alt: "Orange and white cat",
     src: {
       compressed:
-        "https://badger.blob.core.windows.net/photos/mio-gatto-preview.jpg",
-      original: "https://badger.blob.core.windows.net/photos/mio-gatto.jpg",
+        "https://badger.blob.core.windows.net/images/mio-gatto-preview.jpg",
+      original: "https://badger.blob.core.windows.net/images/mio-gatto.jpg",
     },
     title: "Mio Gatto",
     location: "Under the Table",
