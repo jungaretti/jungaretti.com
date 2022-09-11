9cbad3bfe42b802b96a2eba89b45506c2c8a33f8
JP Ungaretti <hello@jungaretti.com>
2022-09-10 20:47:27 -0700

    Use interface instead of class

--- src/data/types/HeadMeta.ts
+++ src/data/types/HeadMeta.ts
@@ -1,13 +1,12 @@
-export class HeadMeta {
-  constructor(
-    public title: string,
-    public description: string,
-    public imageSrc?: string,
-    public permalinkSrc?: string
-  ) {}
+export interface HeadMeta {
+  title: string;
+  description: string;
+  imageSrc?: string;
+  permalinkSrc?: string;
 }
 
-const defaultTitle = "JP Ungaretti";
-const defaultDescription =
-  "Developer, photographer, and cheese curd connoisseur in the Pacific Northwest. Check out my recent work or get in touch with me.";
-export const defaultHeadMeta = new HeadMeta(defaultTitle, defaultDescription);
+export const defaultHeadMeta = {
+  title: "JP Ungaretti",
+  description:
+    "Developer, photographer, and cheese curd connoisseur in the Pacific Northwest. Check out my recent work or get in touch with me.",
+};
