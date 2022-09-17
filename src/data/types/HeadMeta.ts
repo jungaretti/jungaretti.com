d0889704e7bae9bc9947e49a9d84d0a5162050c8
JP Ungaretti <hello@jungaretti.com>
2022-09-16 22:10:49 -0700

    Remove default metadata

--- src/data/types/HeadMeta.ts
+++ src/data/types/HeadMeta.ts
@@ -4,9 +4,3 @@ export interface HeadMeta {
   imageSrc?: string;
   permalinkSrc?: string;
 }
-
-export const defaultHeadMeta = {
-  title: "JP Ungaretti",
-  description:
-    "Developer, photographer, and cheese curd connoisseur in the Pacific Northwest. Check out my recent work or get in touch with me.",
-};
