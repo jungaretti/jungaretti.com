fcb3c448bbce1ae0065060a1375e8a4938528d9d
JP Ungaretti <git@jungaretti.com>
2023-03-13 21:39:03 -0700

    Add lib path

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { artSchema } from "../lib/Art";
 import { photoSchema } from "../lib/Photo";
 import { nationalParkSchema } from "../lib/NationalPark";
