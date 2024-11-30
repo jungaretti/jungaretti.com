4222c1e11fe4a1a97303c1d959a662671999cde0
JP Ungaretti <jp@jungaretti.com>
2024-11-30 14:56:13 -0800

    Tweak deployment script

--- tools/deploy-images.bash
+++ tools/deploy-images.bash
@@ -2,10 +2,19 @@
 
 set -e
 
+account_name="jungaretti"
+container_name="gallery"
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --account-name) account_name="$2"; shift ;;
+        --container-name) container_name="$2"; shift ;;
+        *) break ;;
+    esac
+    shift
+done
+
 function upload_to_azure {
-    local account_name="badger"
-    local container_name="gallery"
-    
     for file in "$@"; do
         local name="${file##*/}"
         az storage blob upload \
@@ -18,32 +27,20 @@ function upload_to_azure {
     done
 }
 
-function deploy_original {
-    for file in "$@"; do
-        local base="${file%.*}"
+for file in "$@"; do
+    base="${file%.*}"
 
-        local jpg="$base.jpg"
-        echo "Converting: $file to $jpg"
-        [ -e "$jpg" ] && echo "$jpg already exists" || convert "$file" -quality 60 "$jpg"
+    original="$base.jpg"
+    echo "Converting: $file to $original"
+    [ -e "$original" ] && echo "$original already exists" || magick convert "$file" -quality 60 "$jpg"
 
-        echo "Uploading: $jpg"
-        upload_to_azure "$jpg"
-    done
-}
+    echo "Uploading: $original"
+    upload_to_azure "$original"
 
-function deploy_preview {
-    for file in "$@"; do
-        local base="${file%.*}"
+    preview="$base-preview.jpg"
+    echo "Converting: $file to $jpg"
+    [ -e "$preview" ] && echo "$preview already exists" || magick convert "$file" -quality 60 -resize 1600x1600 "$original"
 
-        local jpg="$base-preview.jpg"
-        echo "Converting: $file to $jpg"
-        [ -e "$jpg" ] && echo "$jpg already exists" || convert "$file" -quality 60 -resize 1600x1600 "$jpg"
-
-        echo "Uploading: $jpg"
-        upload_to_azure "$jpg"
-    done
-}
-
-az login
-deploy_original "$@"
-deploy_preview "$@"
+    echo "Uploading: $preview"
+    upload_to_azure "$preview"
+done
